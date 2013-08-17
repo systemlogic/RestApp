853dea08d79b89e53788054d5a56c858c1e7ad50
arorah1 <arorah1@emc.com>
2013-08-17 10:22:14 +0530

    resuffle

--- src/main/java/com/common/controller/CheckController.java
+++ src/main/java/com/common/controller/CheckController.java
@@ -9,32 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.common.model.UID;
+
 @Controller
 public class CheckController {
 	@ResponseBody @RequestMapping(value="/description", method = RequestMethod.GET )
 	public UID getDescription(){
 		UID uid = new UID("Pranshi", "Dhingra");
 	    return uid;
-	}
-}
-@XmlRootElement
-class UID {
-	String firstname;
-	String lastName;
-	public UID(String f,String l){
-		firstname = f ;
-		lastName = l;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 }
---
+++ src/main/java/com/common/model/UID.java
@@ -0,0 +1,25 @@
+package com.common.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class UID {
+	String firstname;
+	String lastName;
+	public UID(String f,String l){
+		firstname = f ;
+		lastName = l;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
